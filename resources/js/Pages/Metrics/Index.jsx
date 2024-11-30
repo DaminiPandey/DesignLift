debf04a1875acc8fd3c7251e311ec77a45c6394f
Damini <daminipandey1310@gmail.com>
2024-11-30 23:46:05 +0530

    feat: 3-d cards

--- resources/js/Pages/Metrics/Index.jsx
+++ resources/js/Pages/Metrics/Index.jsx
@@ -3,11 +3,16 @@ import { Head } from "@inertiajs/react";
 import { useState } from "react";
 import axios from "axios";
 import { TextGenerateEffect } from "../../Components/TextGenerateEffect";
- 
+import { useSelector, useDispatch } from "react-redux";
+import { ThreeDCards } from "../../Components/ThreeDCards";
+
 const words = `Oxygen gets you high. In a catastrophic emergency, we're taking giant, panicked breaths. Suddenly you become euphoric, docile. You accept your fate. It's all right here. Emergency water landing, six hundred miles an hour. Blank faces, calm as Hindu cows
 `;
 
 export default function Metrics() {
+    const { analysis } = useSelector((state) => state.analysis);
+    console.log(analysis);
+
     return (
         <AuthenticatedLayout
             header={
@@ -21,8 +26,8 @@ export default function Metrics() {
             <div className="py-12">
                 <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
                     <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">
-                           <TextGenerateEffect duration={2} filter={false} words={words} />
+                        <div className="p-6 text-gray-900 w-full">
+                            <ThreeDCards analysis={analysis} />
                         </div>
                     </div>
                 </div>
