1879976bc6fb3c56a82c9d40d096660e07106637
Riya <riyas@geekyants.com>
2024-12-01 00:07:04 +0530

    added react-d3-speedometer

--- resources/js/Pages/Metrics/Index.jsx
+++ resources/js/Pages/Metrics/Index.jsx
@@ -3,12 +3,29 @@ import { Head } from "@inertiajs/react";
 import { useState } from "react";
 import axios from "axios";
 import { TextGenerateEffect } from "../../Components/TextGenerateEffect";
+import { CardContainer, CardBody, CardItem } from "../../Components/3dcard";
+import { Chart } from "react-google-charts";
  import ReactSpeedometer from "react-d3-speedometer"
+import { useSelector } from "react-redux";
 // and just use it
 const words = `Oxygen gets you high. In a catastrophic emergency, we're taking giant, panicked breaths. Suddenly you become euphoric, docile. You accept your fate. It's all right here. Emergency water landing, six hundred miles an hour. Blank faces, calm as Hindu cows
 `;
+  const data = [
+    ["Task", "Hours per Day"],
+    ["Work", 9],
+    ["Eat", 2],
+    ["Commute", 2],
+    ["Watch TV", 2],
+    ["Sleep", 7],
+  ];
+
+  const options = {
+    title: "My Daily Activities",
+  };
 
 export default function Metrics() {
+    const { analysis } = useSelector((state) => state.analysis);
+    console.log(analysis,"analysis")
     return (
         <AuthenticatedLayout
             header={
@@ -24,12 +41,56 @@ export default function Metrics() {
                     <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900">
                            <TextGenerateEffect duration={2} filter={false} words={words} />
+                            <CardContainer className="inter-var">
+      <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+        <CardItem
+          translateZ="50"
+          className="text-xl font-bold text-neutral-600 dark:text-white"
+        >
+          Make things float in air
+        </CardItem>
+        <CardItem
+          as="p"
+          translateZ="60"
+          className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+        >
+          Hover over this card to unleash the power of CSS perspective
+        </CardItem>
+        <CardItem translateZ="100" className="w-full mt-4">
+                <Chart
+                                chartType="PieChart"
+                                data={data}
+                                options={options}
+                                width={"100%"}
+                                height={"400px"}
+                            />
+        </CardItem>
+        <div className="flex justify-between items-center mt-20">
+          <CardItem
+            translateZ={20}
+            href="https://twitter.com/mannupaaji"
+            target="__blank"
+            className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+          >
+            Try now →
+          </CardItem>
+          <CardItem
+            translateZ={20}
+            as="button"
+            className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
+          >
+            Sign up
+          </CardItem>
+        </div>
+      </CardBody>
+    </CardContainer>
+                           
 
                         </div>
                     </div>
                 </div>
             </div>
-<ReactSpeedometer />
+<ReactSpeedometer minValue={0} maxValue={10} value={5} />
 
         </AuthenticatedLayout>
     );
